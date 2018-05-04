280929cc7c9c3712db8b8559cd7832911477c1a2
Andreas Veithen <andreas.veithen@gmail.com>
2018-05-04 09:25:06 +0100

    Remove unnecessary imports

--- src/main/java/com/github/veithen/cosmos/maven/emf/GenerateMojo.java
+++ src/main/java/com/github/veithen/cosmos/maven/emf/GenerateMojo.java
@@ -27,8 +27,6 @@ import java.util.Map;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.eclipse.emf.codegen.ecore.generator.Generator;
